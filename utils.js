fef63ca48b9f729903e23a1d07118903e4208246
Xuefeng Xu <xuxf1100@gmail.com>
2025-03-30 02:07:59 +0100

    Use Quarto

--- utils.js
+++
@@ -1,5 +0,0 @@
-function displayLastModifiedDate() {
-    var lastModifiedDate = new Date(document.lastModified);
-    var formattedDate = lastModifiedDate.toDateString();
-    document.write("Last Modified Date: " + formattedDate);
-}
